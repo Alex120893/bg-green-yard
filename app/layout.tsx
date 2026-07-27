54c7e09e7e96f50f16d9c55df4310e33c6caac9b
Alex120893 <bg.greenyard@gmail.com>
2026-07-27 17:35:01 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,3 +81,12 @@ export default function RootLayout({
     </html>
   );
 }
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LQMY7RMSV1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LQMY7RMSV1');
+</script>
