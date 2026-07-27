99a9ca7c5a3235375f9ef8e7ad7a409b376d78aa
Alex120893 <bg.greenyard@gmail.com>
2026-07-27 18:05:58 +0300

    Update sitemap.ts with new domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://bg-green-yard.vercel.app',
+      url: 'https://bg-greenyard.com',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
